588913f5c2f6a930470657fa878f62e36124f094
sangeethathallapelly <146064909+sangeethathallapelly@users.noreply.github.com>
2023-09-27 17:31:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function varExampleSample(){
+    if(true){
+        var a="name";
+        console.log(a);
+        a="newname";
+        console.log(a);
+        a="secondnewname";
+    }
+    console.log(a);
+}
+varExampleSample();
+function letExample(){
+    if(true){
+        let b="place";
+        console.log(b);
+        b="newplace";
+        console.log(b);
+        b="anothernewplace";
+        console.log(b);
+    }
+}
+letExample();
+function constExample(){
+    const x="thing";
+    console.log(x);
+}
+constExample();
